f6da1f1764c914255c21d5cad7b8f1b4dc070efc
Viktor <sev-vic@yandex.com>
2023-07-03 01:12:02 +0200

    Homework 22.06

---
+++ src/main/java/lesson2/homework2/Homework.java
@@ -0,0 +1,59 @@
+package lesson2.homework2;
+
+/**
+ * Algorithms. Homework #2
+ *
+ * @author Viktor Uvarchev
+ * @version 02 Jul 2023
+*/
+
+/*
+ * task #1
+ * START
+ * READ number n
+ * IF n == 1 THEN return
+ * FOR i = 1, i <= n, i + 1
+ * 	FOR j = 1; j <= n, j + 1
+ * 		print "*"
+ * 		BREAK
+ * END
+ *
+ *
+ * task #2
+ * START
+ * READ number n
+ * numbers i = 0, j = 0, a = 0
+ * FOR i = n/2, i <= n; i + 1
+ * 	FOR j = 2, j <= n, j * 2
+ * 		a = a + n / 2
+ * END
+ *
+ *
+ * task #3
+ * START
+ * READ number n
+ * number a = 0
+ * FOR i = 0, i < n, i + 1
+ * 	FOR j = n, j > i, j - 1
+ * 		a = a + i + j
+ * END
+ *
+ *
+ * task #4
+ * START
+ * READ number n
+ * numbers a = 0, i = n
+ * WHILE i > 0
+ * 	a = a + i
+ * 	i = i / 2
+ * END
+ */
+
+
+
+public class Homework {
+    // task #1 - Answer: O(n)
+    // task #2 - Answer: O(n*log(n))
+    // task #3 - Answer: O(n^n)
+    // task #4 - Answer: O(log(n))
+}
